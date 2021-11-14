cab859ed902aae6826ea4498a7b77b90a76034d6
MACnusBET7 <63110948+MACnusBET7@users.noreply.github.com>
2021-11-14 22:21:30 +0100

    Init Bouncers

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private static RigidBody rb;
+    
+    public class Movement{
+        private 
+    }
+    
+}
